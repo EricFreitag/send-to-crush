2ce0f4e4ccfffaeca2136c3ad1104b7f6f95acaf
Eric Ellis Freitag <eef49@cornell.edu>
2024-09-24 00:53:50 -0400

    Eric's changes

--- send-to-crush/script.js
+++ send-to-crush/script.js
@@ -5,8 +5,10 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  question.innerHTML = "YAY! I LOVE YOU TOO!";
+  gif.src = "https://media.giphy.com/media/MDJ9IbxxvDUQM/giphy.gif"
+  noBtn.style.display = 'none';
+  yesBtn.style.display = 'none';
 });
 
 // Make the No button move randomly on hover
